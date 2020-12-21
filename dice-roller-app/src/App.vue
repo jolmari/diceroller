9f4ce29894c1580561b4b3be59a91985b27ea23b
Kristian Virmo <kristian.virmo@digia.com>
2020-12-21 21:15:01 +0200

    Vue-app scaffolding
    Initial UI structure & setup

---
+++ dice-roller-app/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="main-container">
+    <div id="navbar">
+      <div id="logo-container">
+        <img id="logo" alt="Diceroller logo" src="./assets/logo.png" />
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="col col-2 col-mx-auto">
+        <div class="dice-panel">
+          Foobar
+        </div>
+      </div>
+      <div class="col col-2 col-mx-auto">
+        <div class="dice-panel">
+          Foobar
+        </div>
+      </div>
+      <div class="col col-2 col-mx-auto">
+        <div class="dice-panel">
+          Foobar
+        </div>
+      </div>
+      <div class="col col-2 col-mx-auto">
+        <div class="dice-panel">
+          Foobar
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
+
+<style lang="scss">
+html {
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+#main-container {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+
+  & > div#navbar {
+    flex: 1;
+    height: 5rem;
+    margin-bottom: 2rem;
+    padding: 0.5rem;
+    background-color: black;
+
+    & > div#logo-container {
+      display: inline-block;
+      width: 5em;
+      height: 5em;
+
+      & > img#logo {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  & div.dice-panel {
+    height: 5rem;
+    padding: 1rem;
+    background-color: red;
+  }
+}
+</style>
---
+++ dice-roller-app/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import '../node_modules/spectre.css/dist/spectre.min.css';
+
+createApp(App).mount("#app");
