b50a0e90eae8ad892cffd27695abf3b4e88fbc45
Kristian Virmo <kristian.virmo@digia.com>
2020-12-24 15:55:13 +0200

    Added dice roll retrieval method & table storage saving

--- DiceRollerFunctions/DiceCalculationFunction.cs
+++ DiceRollerFunctions/DiceCalculationFunction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,15 +9,17 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
 using Newtonsoft.Json;
 
 namespace DiceRollerFunctions
 {
     public static class DiceCalculationFunction
     {
-        [FunctionName("DiceCalculationFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "calculate")] HttpRequest req)
+        [FunctionName("CalculateDicePoolRoll")]
+        public static async Task<IActionResult> CalculateDicePoolRoll(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "calculate")] HttpRequest req,
+            [Table("DiceRolls")] CloudTable cloudTable)
         {
             using (var reader = new StreamReader(req.Body))
             {
@@ -27,8 +30,37 @@ namespace DiceRollerFunctions
                     .SelectMany(r => r.Roll())
                     .OrderBy(r => r.Sides);
 
+                if (!result.Any())
+                {
+                    return new OkResult();
+                }
+
+                // Insert roll to table storage. Add RowKey as a timestamp decremented from
+                // max value. This guarantees that the table insertion order is newest first.
+                var insertOperation = TableOperation.Insert(new DiceRollRecord
+                {
+                    PartitionKey = "<UserName>",
+                    RowKey = (DateTime.MaxValue - DateTime.UtcNow).Ticks.ToString("d19"),
+                    JsonDiceRollRecord = JsonConvert.SerializeObject(result)
+                });
+
+                await cloudTable.ExecuteAsync(insertOperation);
+
                 return new OkObjectResult(result);
             }
+        }
+
+        [FunctionName("RetrieveDicePoolRoll")]
+        public static async Task<IActionResult> RetrieveDicePoolRoll(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "retrieve")] HttpRequest req,
+            [Table("DiceRolls")] CloudTable cloudTable)
+        {
+            var tableQuery = new TableQuery<DiceRollRecord>();
+            var queryResult = await cloudTable.ExecuteQuerySegmentedAsync(tableQuery, null);
+
+            return new OkObjectResult(queryResult
+                .Where(r => !string.IsNullOrWhiteSpace(r.JsonDiceRollRecord))
+                .Select(r => JsonConvert.DeserializeObject<List<DiceRollResult>>(r.JsonDiceRollRecord)));
         }
     }
 }
